7f7a52bf74aaed540a25d0aa4cef3eb6886c0a0b
Corbin Page <corbin.page@gmail.com>
2015-05-21 20:59:52 -0400

    Hello World Extension is working

--- content.js
+++ content.js
@@ -1,15 +1,48 @@
 InboxSDK.load(1, 'sdk_appian-tasks_8f26985331').then(function(sdk) {
 
-	var routeID = 'helloWorld/:country';
+	var routeID = 'appian/tasks/assignedtome';
 
 	sdk.Router.handleCustomRoute(routeID, function(customRouteView){
-		customRouteView.getElement().textContent = 'hello world! ' + customRouteView.getParams().country;
-	});
+		var large = '<script src="https://appswest.princetonblue.net:8443/suite/tempo/tempo.nocache.js"></script>' +
+								'<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>' +
+								'<script>' +
+								'  /* This function dynamically inserts an <appian-task> tag into the page with the user-specified taskId */' +
+								'  function addTask() {' +
+								'    var taskId = $("#new-task").val();' +
+								'    if (taskId) {' +
+								'      $("<appian-task />").attr("taskId", taskId).on("submit", handleSubmit).appendTo("#taskContent");' +
+								'    }' +
+								'  }' +
+								'  /* This function is called by the submit event listener */' +
+								'  function handleSubmit() {' +
+								'    alert("The task has been submitted!");' +
+								'  }' +
+								'</script>' +
+								'<div id="inputs">' +
+								'  <input id="new-task" /><button onclick="return addTask();">New Task</button>' +
+								'</div>' +
+								'<div id="taskContent"></div>';
+
+		var contentDiv = customRouteView.getElement();
+
+		$.get('taskDataJson.json', function(jsonData) {
+			console.log(jsonData);
+						contentDiv.innerText = jsonData;
+			   		// $('#div').html(data.replace('n','<br />'));
+					},'text');
+
+
+		// contentDiv.innerHTML = large;
+
+
+
+
+			});
 
 	sdk.NavMenu.addNavItem({
-		name: 'Hello Canada',
+		name: 'Appian - My Tasks',
 		routeID: routeID,
-		routeParams: {country: 'Canada'}
+		routeParams: {state: 'Testing'}
 	});
 
 });
